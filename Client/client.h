2c2fac2336d2c467b7b2ec4d2acddf44d9369391
wendelpena <wendel.pena@hotmail.com>
2023-02-18 16:47:30 -0800

    Client edit2

--- Client/client.h
+++ Client/client.h
@@ -1,3 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
+	
+#define PORT	 8080
+#define MAXLINE 1024
+
 int socket(int domain, int type, int protocol);
 //Creates an unbound socket in the specified domain.
 //Returns socket file descriptor.
@@ -9,5 +21,8 @@ ssize_t sendto(int sockfd, const void *buf, size_t len, int flags,
 ssize_t recvfrom(int sockfd, void *buf, size_t len, int flags,
                  struct sockaddr *src_addr, socklen_t *addrlen);
 //Receive a message from the socket.
+
+int close(int fd);
+//Close a file descriptor
 
 
--- main.c
+++ main.c
@@ -4,4 +4,5 @@
 
 int main(void) {
   printf("Hello, world!");
+  printf("%d\n", getNum());
 }
